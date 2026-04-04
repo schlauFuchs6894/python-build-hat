fc67d91b8e1c468a68d93085cf2435b30b5b1159
xy <xy@xy.com>
2026-04-04 15:49:22 +0200

    Expand Hat Tests

--- test/hat.py
+++ test/hat.py
@@ -15,9 +15,22 @@ class TestHat(unittest.TestCase):
         self.assertGreaterEqual(vin, 7.2)
         self.assertLessEqual(vin, 8.5)
 
+    def test_vin(self):
+        """Test (init by value) voltage measure function"""
+        h = Hat(
+            device="/dev/ttyAMA0",
+            reset_gpio=4,
+            boot0_gpio=22,
+            debug=False,
+        )
+        vin = h.get_vin()
+        self.assertGreaterEqual(vin, 7.2)
+        self.assertLessEqual(vin, 8.5)
+
     def test_get(self):
         """Test getting list of devices"""
         h = Hat()
+        print("h.get()")
         self.assertIsInstance(h.get(), dict)
 
     def test_serial(self):
--- test/hat2.py
+++ test/hat2.py
@@ -10,14 +10,25 @@ class TestHat(unittest.TestCase):
 
     def test_vin(self):
         """Test voltage measure function"""
-        h = Hat()
+        h = Hat(
+            device="/dev/ttyAMA4",
+            reset_gpio=25,
+            boot0_gpio=24,
+            debug=False,
+        )
         vin = h.get_vin()
         self.assertGreaterEqual(vin, 7.2)
         self.assertLessEqual(vin, 8.5)
 
     def test_get(self):
         """Test getting list of devices"""
-        h = Hat()
+        h = Hat(
+            device="/dev/ttyAMA4",
+            reset_gpio=25,
+            boot0_gpio=24,
+            debug=False,
+        )
+        print("h.get()")
         self.assertIsInstance(h.get(), dict)
 
     def test_serial(self):
