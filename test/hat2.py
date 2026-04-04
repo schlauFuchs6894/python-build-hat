5de49a6ecc51a6503e16b0f22e0214db42414c0c
xy <xy@xy.com>
2026-04-04 18:21:16 +0200

    Test Hat1 Hat2

--- test/hat2.py
+++ test/hat2.py
@@ -3,10 +3,12 @@
 import unittest
 
 from buildhat import Hat
-
+import logging
 
 class TestHat(unittest.TestCase):
     """Test hat functions"""
+
+
 
     def test_vin(self):
         """Test voltage measure function"""
@@ -19,17 +21,40 @@ class TestHat(unittest.TestCase):
         vin = h.get_vin()
         self.assertGreaterEqual(vin, 7.2)
         self.assertLessEqual(vin, 8.5)
-
+ 
     def test_get(self):
+        # Read HAT 2
         """Test getting list of devices"""
-        h = Hat(
+        rstH1 = OutputDevice(4, active_high=True, initial_value=True)
+        print("Reset high")
+        rstH1.off()
+ 
+        h2 = Hat(
             device="/dev/ttyAMA4",
             reset_gpio=25,
             boot0_gpio=24,
             debug=False,
         )
+        logging.basicConfig(level=logging.INFO)
         print("h.get()")
-        self.assertIsInstance(h.get(), dict)
+        logging.info("HAT 2:")
+        logging.info(h2.get())
+        self.assertIsInstance(h2.get(), dict)
+        
+        # Read HAT 1
+        rstH2 = OutputDevice(25, active_high=True, initial_value=True)
+        print("Reset high")
+        rstH2.off()
+
+        h1 = Hat(
+            device="/dev/ttyAMA0",
+            reset_gpio=4,
+            boot0_gpio=22,
+            debug=False,
+        )    
+        logging.info("HAT 1:")
+        logging.info(h1.get())
+        self.assertIsInstance(h1.get(), dict)
 
     def test_serial(self):
         """Test setting serial device"""
