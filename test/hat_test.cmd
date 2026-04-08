1d48823bde5c02d802eabdb0acb39322d8a3ec0f
xy <xy@xy.com>
2026-04-09 00:10:59 +0200

    fix

--- test/hat_test.cmd
+++ test/hat_test.cmd
@@ -16,12 +16,14 @@ EOF
 #############################################################
 
 python - <<'EOF'
+import time
 from buildhat import Hat, Motor, ColorDistanceSensor
 from gpiozero import OutputDevice
 # Read HAT 1
-rstH2 = OutputDevice(25, active_high=True, initial_value=True)
-print("H2 GPIO25 Reset high")
+rstH2 = OutputDevice(5, active_high=True, initial_value=True)
+print("H2 GPIO5 Reset high")
 rstH2.off()
+
 
 h1 = Hat(
     device="/dev/ttyAMA0",
@@ -34,29 +36,23 @@ print(h1.get())
 
 h1=Hat()
 print(h1.get())
-
-EOF
-
-
+time.sleep(0.5)  # wait for HAT to boot after reset
 
 #############################################################
 
-
-python - <<'EOF'
-from buildhat import Hat, Motor, ColorDistanceSensor
-from gpiozero import OutputDevice
-
 # Read HAT 2
 """Test getting list of devices"""
-rstH2 = OutputDevice(25, active_high=True, initial_value=False)
-rstH1 = OutputDevice(4, active_high=True, initial_value=True)
+rstH1 = OutputDevice(4, active_high=True, initial_value=False)
 print("H1 GPIO4 Reset high")
 rstH1.off()
+rstH2.on()
+time.sleep(0.5)  # wait for HAT to boot after reset
+del rstH2
 
 h2 = Hat(
     device="/dev/ttyAMA4",
-    reset_gpio=05,
-    boot0_gpio=06,
+    reset_gpio=5,
+    boot0_gpio=6,
     debug=False,
 )
 print("HAT2:")
